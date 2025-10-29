51727ab75bdf0c40d35a013857c562fe9fc9be85
Luke Stevens <lukestevens100@gmail.com>
2025-10-29 19:25:23 +0000

    fix: updated SignInServlet login validation

--- SignInServlet.java
+++ SignInServlet.java
@@ -1,3 +1,22 @@
-public class SignInServlet {
-    
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class SignInServlet extends HTTPServlet{
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+        throws IOException, ServletException{
+
+            String username = request.doGet("username");
+
+            String password = request.doGet("password")
+
+            response.setContentType("text/html");
+            PrintWriter out = response.getWriter();
+
+        
+
+    }
 }

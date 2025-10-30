b63938592f144ac2874bdee11c347aab57cda0e6
Luke Stevens <lukestevens100@gmail.com>
2025-10-30 15:50:44 +0000

    Initial commit: FrontPage start

--- SignInServlet.java
+++ SignInServlet.java
@@ -6,17 +6,20 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class SignInServlet extends HTTPServlet{
+
+    @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response)
         throws IOException, ServletException{
 
             String username = request.doGet("username");
 
-            String password = request.doGet("password")
+            String password = request.doGet("password");
 
             response.setContentType("text/html");
             PrintWriter out = response.getWriter();
 
-        
+            out.println("<html>");
+            out.println("<head><>");
 
     }
 }
